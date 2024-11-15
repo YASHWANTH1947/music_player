767f4b19f59b0f9e0e2a8ff8ba25a040106bc99e
Yashwanth <yashwanthreddy1947@gmail.com>
2024-11-15 20:38:35 +0530

    slider bar is done

--- apps.js
+++ apps.js
@@ -1,3 +1,32 @@
-console.log("hello");
-const song = new Audio(url('songs/1.mp3'));
-song.play()
+const masterPlay=document.querySelector('#masterPlay')
+const audio1=new Audio('songs/1.mp3');
+const gif=document.getElementById('gifInfo');
+const seeker=document.querySelector("#myprogressbar");
+
+
+
+
+
+masterPlay.addEventListener('click',()=>{
+    if(audio1.paused || audio1.currentTime<=0){
+    audio1.play();
+    masterPlay.classList.remove("fa-circle-play");
+    masterPlay.classList.add("fa-circle-pause");
+ 
+    gif.style.opacity=1;
+}
+else{
+    audio1.pause();
+    masterPlay.classList.remove("fa-circle-pause");
+    masterPlay.classList.add("fa-circle-play");
+    gif.style.opacity=0;
+}
+})
+audio1.addEventListener('timeupdate',()=>{
+    progress=(audio1.currentTime/audio1.duration)*100;
+    seeker.value=parseInt(progress);
+
+})
+seeker.addEventListener('change',()=>{
+    audio1.currentTime=(seeker.value*audio1.duration)/100
+})
